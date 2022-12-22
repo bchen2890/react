7d594be4df87193ad34a2eaa9264fb71fb0e2b58
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-12-22 21:07:19 +0100

    Year filter

--- tutorial-app/src/components/List/List.js
+++ tutorial-app/src/components/List/List.js
@@ -1,10 +1,24 @@
 import  ListItem from "./ListItem"
 import './List.css';
+import ListFilter from "./ListFilter";
+import { useState } from "react";
 
 function List({ items }) {
+  const [filteredYear, setFilteredYear] = useState('2022');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
 	<div className='list'>
-        {items.map(i=> <ListItem date={i.date} name={i.name} price={i.price}/>)}
+      <ListFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map(i=> <ListItem date={i.date} name={i.name} price={i.price}/>)}
 	</div>
   )
 }
