f6cbd0698f5f24b282c39c7ae006fb3e01a869b0
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2023-01-05 17:25:43 +0100

    Try Styled-components

--- tutorial-app/src/components/List/ListItem.js
+++ tutorial-app/src/components/List/ListItem.js
@@ -1,11 +1,26 @@
 import './ListItem.css'
 
+import styled from 'styled-components';
+
+const ListComponent = styled.div`
+  @media (max-width: 768px) {
+    background-color: #71cfca;
+    .list-item-date {
+        border-right: 1px solid #008B8B;
+    }
+      
+    .list-item-price {
+        border-left: 1px solid #7abfbf;
+    }
+  }
+`;
+
 function ListItem (props) {
-    return <div className='list-item'>
+    return <ListComponent className='list-item'>
         <div className='list-item-date'>{new Date(props.date).toLocaleDateString()}</div>
         <h2  className='list-item-name'>{props.name}</h2>
         <div className='list-item-price'>{props.price}</div>
-    </div>
+    </ListComponent>
 }
 
 export default ListItem;
